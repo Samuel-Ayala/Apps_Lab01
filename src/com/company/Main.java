a97ac38e67b5dc20659626c1c0225c06ecf9c21e
Samuel-Ayala <a20166262@pucp.edu.pe>
2020-09-12 18:31:20 -0500

    opcion3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
@@ -19,7 +20,6 @@ public class Main {
             {
                 case "1":
                 {
-                    
                     break;
                 }
                 case "2":
@@ -28,7 +28,19 @@ public class Main {
                 }
                 case "3":
                 {
-
+                    for ( Hada hada : listaDeHadas) {
+                        int i = 1;
+                        System.out.println("Lista de hadas: ");
+                        System.out.println(i + ") " + "Nombre: " + hada.getNombre() + " - " + "Edad: " + hada.getEdad());
+                        i++;
+                    }
+                    System.out.println("----------------------------------------------------------------------");
+                    for ( Humano humano : listaDeHumanos) {
+                        int ii = 1;
+                        System.out.println("Lista de humanos: ");
+                        System.out.println(ii + ") " + "Nombre: " + humano.getNombre() + " - " + "Edad: " + humano.getEdad());
+                        ii++;
+                    }
                     break;
                 }
                 case "4":
