def7459b0847d3880e36f9a5b3196ec3ddad747c
Alvaro Burga <a20150632@pucp.pe>
2020-09-12 18:07:50 -0500

    con las clases

---
+++ src/com/company/Humano.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class Humano {
+    
+    private String nombre;
+    private int edad;
+    private Hada padrino;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * @return the padrino
+     */
+    public Hada getPadrino() {
+        return padrino;
+    }
+
+    /**
+     * @param padrino the padrino to set
+     */
+    public void setPadrino(Hada padrino) {
+        this.padrino = padrino;
+    }
+    
+}
