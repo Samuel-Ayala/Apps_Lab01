346bd2585d03a5e1ab38c198c22d7ffa87abb5f8
Marcelo Garcia <m.garciav@pucp.edu.pe>
2020-09-12 18:56:47 -0500

    parte2_Marcelooo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,7 +24,49 @@ public class Main {
                 }
                 case "2":
                 {
+                    Humano human = new Humano();
+                    //llenamos todos los datos
+                    System.out.print("Ingrese el nombre: ");
+                    String nombre1 = sc.nextLine();
+                    if (nombre1.length() <= 20) {
+                        try {
+                            int x = Integer.valueOf(nombre1);
+                            //break nombre;             
+                        }
+
+                     catch (NumberFormatException e) {  
+                         human.setNombre(nombre1);
+                         //System.out.println("nombre con numero y letras");
+
+                    }
+                    }
+                    System.out.print("Ingrese la edad: ");
+                    String edad1 = sc.nextLine();
+                     try {
+                        if ((Integer.valueOf(edad1)> 5)&&(Integer.valueOf(edad1) < 12)) {
+
+                            throw new NumberFormatException();
+                            //System.out.println("nombre no valido");
+                        } 
+                         System.out.println("edad no valida");
+                    } catch (NumberFormatException e) {  
+                        human.setEdad(Integer.valueOf(edad1));   
+                    }
                     
+                    System.out.print("Ingrese el nombre del padrino: ");
+                    String nombre2 = sc.nextLine();
+                    for(Hada pepe : listaDeHadas){
+                        if(pepe.getNombre().equals(nombre2)){
+                            if(pepe.getAhijado().getNombre().equals(nombre1)){
+                                human.setPadrino(pepe);
+                            }
+                        }
+                       }
+                     
+                    //
+                    listaDeHumanos.add(human);
+                    break;
+
                 }
                 case "3":
                 {
